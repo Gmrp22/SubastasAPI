6a30e3a46f346fbfac4e90448208c74631427601
Gilda <gmrp22@hotmail.com>
2020-11-17 13:03:14 -0600

    CRUD oferta

--- SubastasAPI/apps/Oferta/urls.py
+++ SubastasAPI/apps/Oferta/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 urlpatterns = [
-    #path('Ofertas/', .as_view()),
+    path('Ofertas/', ListaOfertas.as_view()),
+    path('Ofertas/POST', OfertaPost.as_view()),
+    path('Ofertas/DELETE/<int:pk>/', OfertaDelete.as_view()),
+    path('Ofertas/PUT/<int:pk>/', OfertaPut.as_view()),    #path('Ofertas/', .as_view()),
 
 
 ]
--- SubastasAPI/apps/Oferta/views.py
+++ SubastasAPI/apps/Oferta/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveDestroyAPIView, RetrieveUpdateAPIView
+from rest_framework.views import APIView
+from .models import Oferta
+from .serializers import OfertaSerializer
+from rest_framework import status
 # Create your views here.
+
+
+class ListaOfertas(ListAPIView):
+    """ Lista todas las subastas """
+    queryset = Oferta.objects.all()
+    serializer_class = OfertaSerializer
+
+
+class OfertaPost(CreateAPIView):
+    """ Crea una subasta """
+    queryset = Oferta.objects.all()
+    serializer_class = OfertaSerializer
+
+
+class OfertaPut(RetrieveUpdateAPIView):
+    """ Actualizar subasta"""
+    queryset = Oferta.objects.all()
+    serializer_class = OfertaSerializer
+    lookup_field = 'pk'
+
+
+class OfertaDelete(RetrieveDestroyAPIView):
+    """ Eliminar una subasta """
+    queryset = Oferta.objects.all()
+    serializer_class = OfertaSerializer
+    lookup_field = 'pk'
